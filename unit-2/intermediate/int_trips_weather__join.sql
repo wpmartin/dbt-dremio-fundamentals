2709ec489ef64a954fc8855de2f5e50c5ca9c7a2
Will Martin <will.martin@dremio.com>
2025-04-08 11:30:17 +0100

    Removed weather models

--- unit-2/intermediate/int_trips_weather__join.sql
+++
@@ -1,3 +0,0 @@
-SELECT * 
-FROM 
-    {{ ref('int_trips__formatted') }} as t INNER JOIN {{ ref('int_weather__formatted') }} as w ON t.pickup_date = w."date";
--- unit-2/marts/nyc_temperature.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    station,
-    "date",
-    temp_max,
-    temp_min
-FROM
-    nessie.nyc.intermediate.int_weather__formatted
